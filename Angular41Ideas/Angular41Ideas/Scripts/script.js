115c2c1bb4630c061f999a28a6a793de4a5d039e
bryantt23 <bryantt23@DESKTOP-7I0G27J>
2018-06-03 03:03:16 -0700

    Added calculator using ng-show and ng-hide

--- Angular41Ideas/Angular41Ideas/Scripts/script.js
+++ Angular41Ideas/Angular41Ideas/Scripts/script.js
@@ -4,4 +4,18 @@ var app = angular.module("projects", []);
 
 app.controller("projectsCtrl", function ($scope) {
     $scope.msg = "hello";
+
+    $scope.numbersAdded = false;
+    $scope.numbersMultiplied = false;
+
+    $scope.addNumbers = function () {
+        $scope.numbersAdded = true;
+        $scope.numbersMultiplied = false;
+    }
+
+    $scope.multiplyNumbers = function () {
+        $scope.numbersAdded = false;
+        $scope.numbersMultiplied = true;
+    }
+
 })
